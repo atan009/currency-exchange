5aab1161dd20ac7758b35e4e50de2325c461c347
atan009 <atan009@users.noreply.github.com>
2017-08-18 16:08:19 -0400

    click changes input/output

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 	var url = "https://forex.1forge.com/1.0.2/symbols?api_key=" + api_key;
 
 	app.controller('OptionController', function($scope){
-		alert("ran");
+
 		// $.getJSON(url, function(data) {
 		// 	$scope.$apply(function(){
 		// 		$scope.listOptions = data;
@@ -15,12 +15,23 @@
 		// 	alert("Could not request url");
 		// });
 		// console.log(self.listOptions);
-		// $scope.listOptions = ["1", "2", "3"];
+		$scope.listOptions = ["USDEUR", "2", "3"];
 	});
 
-   $(document).ready(function() {
+    $(document).ready(function() {
    		// converts list into select2 style list
 		$("#convert-list").select2({});
 	});
 
+    app.controller('ConversionController', function(){
+    	this.convertFunction = function() {
+    		var amt = $(".convert-amount").val();
+    		var selType = "" + $(".select2-selection__rendered").html();
+    		var fromType = selType.slice(0,3);
+    		var toType = selType.slice(3,6);
+
+    		$(".col-md-6 h2").html("Currency Conversion: " + fromType + " &#8594 " + toType);
+    		console.log($(".col-md-6 h2").html());
+    	}
+    });
 })();
